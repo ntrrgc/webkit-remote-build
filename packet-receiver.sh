2869e86890b7b1552807eb92370246a956614652
Alicia Boya García <ntrrgc@gmail.com>
2017-09-18 22:39:32 +0200

    Place heredoc in correct place

--- packet-receiver.sh
+++ packet-receiver.sh
@@ -5,10 +5,9 @@ DIR="$(dirname $(realpath "$0"))"
 . "$DIR/config.sh"
 
 local_cores="$(getconf _NPROCESSORS_ONLN)"
-ssh -T "$BUILD_HOST" \
+ssh -T "$BUILD_HOST" <<END \
   | "$DIR/packet-splitter.sh" \
   | xargs -P$local_cores -d"\n" -n1 "$DIR/extract-packet.sh" \
-  <<END
 set -eu
 rm -f /tmp/delta-socket
 ${REMOTE_SCRIPTS_DIR@Q}/listen-socket.py /tmp/delta-socket | mbuffer -m 200M -q
