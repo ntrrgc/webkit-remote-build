f966c2571be81025270c718038e350ffb3b6612e
Alicia Boya García <ntrrgc@gmail.com>
2017-09-18 22:46:07 +0200

    Remove spurious backslash

--- packet-receiver.sh
+++ packet-receiver.sh
@@ -7,7 +7,7 @@ DIR="$(dirname $(realpath "$0"))"
 local_cores="$(getconf _NPROCESSORS_ONLN)"
 ssh -T "$BUILD_HOST" <<END \
   | "$DIR/packet-splitter.sh" \
-  | xargs -P$local_cores -d"\n" -n1 "$DIR/extract-packet.sh" \
+  | xargs -P$local_cores -d"\n" -n1 "$DIR/extract-packet.sh"
 set -eu
 rm -f /tmp/delta-socket
 ${REMOTE_SCRIPTS_DIR@Q}/listen-socket.py /tmp/delta-socket | mbuffer -m 200M -q
