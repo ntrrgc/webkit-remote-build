0d87c76967da6562132851eb8b968b03c9827936
Alicia Boya García <ntrrgc@gmail.com>
2017-09-18 20:46:35 +0200

    Change REMOTE_SCRIPTS_DIR

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@ BUILD_HOST=homura.ntrrgc.me
 BUILD_ARGS=(--gtk --debug --cmakeconfig="-DENABLE_GTK_DOC=OFF")
 REMOTE_BUILD_DIR=WebKitBuild/webm/Debug
 LOCAL_BUILD_DIR=WebKitBuild/webm/Debug
-REMOTE_SCRIPTS_DIR=/home/ntrrgc/Dropbox/obj-compress
+REMOTE_SCRIPTS_DIR=/home/ntrrgc/Apps/webkit-remote-build
