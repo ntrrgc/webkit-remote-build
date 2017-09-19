95c9ebddafe95ff4424190a5ebcb89ea490f07c2
Alicia Boya García <ntrrgc@gmail.com>
2017-09-19 20:25:05 +0200

    Fix gtkdoc flag

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 STORE="$HOME/.webkit-remote"
 BUILD_HOST=homura.ntrrgc.me
-BUILD_ARGS=(--gtk --debug --cmakeconfig="-DENABLE_GTK_DOC=OFF")
+BUILD_ARGS=(--gtk --debug --cmakeconfig="-DENABLE_GTKDOC=OFF")
 REMOTE_BUILD_DIR=WebKitBuild/webm/Debug
 LOCAL_BUILD_DIR=WebKitBuild/webm/Debug
 REMOTE_SCRIPTS_DIR=/home/ntrrgc/Apps/webkit-remote-build
