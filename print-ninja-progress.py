a45e9c0e9c790de462f2b28ee4f069f362ee6906
Alicia Boya García <ntrrgc@gmail.com>
2017-09-19 15:48:16 +0200

    Attempt at multiple progress bars

--- print-ninja-progress.py
+++ print-ninja-progress.py
@@ -4,9 +4,7 @@ import re
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    pbar = None
-    done_before = 0
-
+    bars = {}
     re_progress = re.compile(r"^\[(\d+)/(\d+)\] ")
 
     try:
@@ -16,19 +14,37 @@ if __name__ == "__main__":
                 done = int(match.groups()[0])
                 total = int(match.groups()[1])
 
-                # pbar is initialized only when the first progress line is
+                if total <= 1:
+                    # Ignore short tasks
+                    continue
+
+                # Ideally, build-webkit would provide a hint of what task
+                # this progress corresponds to, which I would use here.
+                # Unfortunately this is not the case. build-webkit launches
+                # several different tasks that write to the same file,
+                # unidentified.
+                # I use the "total" number as a name of sorts to differentiate
+                # different processes. It's not perfect, but better than
+                # nothing.
+                bar_name = total
+
+                # The bar is initialized only when the first progress line is
                 # found.
-                if pbar is None:
-                    pbar = tqdm(total=total, unit="objects")
+                if bar_name not in bars:
+                    bars[bar_name] = tqdm(total=total, unit="objs", ncols=80)
 
-                pbar.total = total
-                pbar.update(done - done_before)
+                if done > bars[bar_name].n:
+                    # Advance the progress bar
+                    bars[bar_name].update(done - bars[bar_name].n)
 
-                done_before = done
+                if done == total:
+                    # Finished with this bar
+                    bars[bar_name].close()
+                    del bars[bar_name]
             else:
-                print(line, end="")
+                if len(bars) == 0:
+                    print(line, end="", flush=True)
+                else:
+                    next(iter(bars.values())).write(line, end="")
     except KeyboardInterrupt:
         pass
-    finally:
-        if pbar is not None:
-            pbar.close()
