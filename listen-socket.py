8edb5e29eefee31aad89ec45e1c8c0b74d948a39
Alicia Boya García <ntrrgc@gmail.com>
2017-09-19 13:58:02 +0200

    Use poll() to stop listening for connections if stdout is closed

--- listen-socket.py
+++ listen-socket.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
+# Listen on the UNIX domain socket specified as parameter, one connection at
+# a time. The data sent from each client is written to stdout.
+#
+# The program exits when stdout is closed.
+#
+# Closing stdout while a client is sending data is an error and will raise a
+# BrokenPipeError.
+#
 import fcntl
 import os
 import socket
 import sys
+import select
 
 sys.stdout = os.fdopen(1, 'wb', 0)
-
-
-def is_stdout_open():
-    try:
-        fcntl.fcntl(1, fcntl.F_GETFL)
-        return True
-    except OSError:
-        return False
 
 
 if __name__ == '__main__':
@@ -21,14 +22,28 @@ if __name__ == '__main__':
         s.bind(sys.argv[1])
         s.listen(1000)
 
-        while is_stdout_open():
-            ss, address = s.accept()
-            while True:
-                block = ss.recv(2048)
-                if len(block) == 0:
-                    ss.close()
-                    break
+        poll = select.poll()
+        poll.register(s.fileno())
+        poll.register(sys.stdout.fileno(), select.POLLERR)
 
-                sys.stdout.write(block)
+        while True:
+            for (fd, event_type) in poll.poll():
+                if fd == sys.stdout.fileno() and event_type == select.POLLERR:
+                    # stdout closed
+                    raise SystemExit(0)
+                elif fd == s.fileno() and event_type == select.POLLIN:
+                    # Connection received
+                    ss, address = s.accept()
+                    while True:
+                        block = ss.recv(2048)
+                        if len(block) == 0:
+                            ss.close()
+                            break
+
+                        sys.stdout.write(block)
+                else:
+                    raise AssertionError(f"Unhandled event: fd={fd} "
+                                         "event_type={event_type}",
+                                         file=sys.stderr)
     except KeyboardInterrupt:
         pass
