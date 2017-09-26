b0d768b4b902dfb594d5a6952dee5143735f411b
Alicia Boya García <ntrrgc@gmail.com>
2017-09-26 15:21:02 +0200

    Add example-irregular-progress.sh

---
+++ example-irregular-progress.sh
@@ -0,0 +1,238 @@
+echo '[1/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[2/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[3/38] dot'
+sleep 0.1s
+echo '[4/38] dot'
+sleep 0.1s
+echo '[5/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[6/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[7/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[8/38] dot'
+sleep 0.1s
+echo '[9/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[10/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[11/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[12/38] dot'
+sleep 0.1s
+echo '[13/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[14/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[15/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[16/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[17/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[18/38] dot'
+sleep 0.1s
+echo '[19/38] dot'
+sleep 0.1s
+echo '[20/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[21/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[22/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[23/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[24/38] dot'
+sleep 0.1s
+echo '[25/38] dot'
+sleep 0.1s
+echo '[26/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[27/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[28/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[29/38] dot'
+sleep 0.1s
+echo '[30/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[31/38] dot'
+sleep 0.1s
+echo '[32/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[33/38] dot'
+sleep 0.1s
+echo '[34/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[35/38] dot'
+sleep 0.1s
+sleep 0.1s
+echo '[36/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[37/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+echo '[38/38] dot'
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
+sleep 0.1s
