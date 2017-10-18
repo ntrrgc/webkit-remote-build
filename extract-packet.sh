32406c17c7e260c4cd19097d45ebe5e7c41af584
Alicia Boya García <ntrrgc@gmail.com>
2017-10-18 20:52:24 +0200

    Remove superfluous variable that ties script unnecessarily to WebKit

--- extract-packet.sh
+++ extract-packet.sh
@@ -6,7 +6,6 @@ DIR="$(dirname $(realpath "$0"))"
 . "$DIR/find-build-dirs.sh"
 
 BASELINE_STORE="$STORE/baseline/$LOCAL_BUILD_DIR"
-DEST_STORE="/webkit/$LOCAL_BUILD_DIR"
 
 # Open file passed as parameter for reading in file descriptor 3
 packet_file="$1"
@@ -26,23 +25,23 @@ read -r file_timestamp <&3
 read method <&3
 
 echo "Receiving $file with method $method ($file_size bytes)" >>/tmp/extract-packet.log
-mkdir -p "$(dirname "$DEST_STORE/$file")"
+mkdir -p "$(dirname "$LOCAL_BUILD_DIR/$file")"
 
 case "$method" in
 "xz")
-  head -c "$file_size" <&3 | xz -d -c > "$DEST_STORE/$file"
+  head -c "$file_size" <&3 | xz -d -c > "$LOCAL_BUILD_DIR/$file"
   ;;
 "delta")
   xdelta -d -f -s "$BASELINE_STORE/$file" \
     <(head -c "$file_size" <&3) \
-    "$DEST_STORE/$file"
+    "$LOCAL_BUILD_DIR/$file"
   ;;
 *)
   echo "Invalid method: $method"
   exit 1
 esac
 
-touch --no-create --time=modify --date="$file_timestamp" "$DEST_STORE/$file"
+touch --no-create --time=modify --date="$file_timestamp" "$LOCAL_BUILD_DIR/$file"
 
 # Close file and delete it
 exec 3>&-
