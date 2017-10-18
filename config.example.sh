2fc55396ff30afa489b6fdb4b8a36469b4c841f5
Alicia Boya García <ntrrgc@gmail.com>
2017-10-18 20:53:46 +0200

    Document configuration in more detail

--- config.example.sh
+++ config.example.sh
@@ -1,12 +1,42 @@
+# Storage directory of this tool. Baseline is stored here (only used for xdelta
+# method)
 STORE="$HOME/.webkit-remote"
-BUILD_HOST=homura.ntrrgc.me
-BUILD_MACHINE_HOSTNAME=homura  # note: only the first part is matched; the domain part, if any, is ignored
-# --debug is added here if DEBUG=1 is specified as an environment variable
-BUILD_ARGS=(--gtk --cmakeconfig="-DENABLE_GTKDOC=OFF")
+
+# Host to connect to using ssh.
+BUILD_HOST=my-dynamic-dns.example.com
+
+# Name of the build machine. Used to know if we are running code there or not.
+# Note: only the first part is matched, so "foo" matches both "foo" and 
+# "foo.bar".
+BUILD_MACHINE_HOSTNAME=example
+
+# Path where these scripts will be installed or updated in the build machine.
 REMOTE_SCRIPTS_DIR=/home/ntrrgc/Apps/webkit-remote-build
-REMOTE_BASH=(bash --login)  # Command used with ssh to get a shell. You can change it e.g. to use a chroot.
+
+# Arguments passed to build-webkit
+BUILD_ARGS=(--gtk --debug --cmakeconfig="-DENABLE_GTKDOC=OFF")
+
+# Path to build directory in the build machine
+REMOTE_BUILD_DIR=/webkit/WebKitBuild/Debug
+
+# Path to build directory in the local machine
+LOCAL_BUILD_DIR=/webkit/WebKitBuild/Debug
+
+# xz compress flags. You can set the compression level here. Higher values
+# compress better but more slowly. For best performance, you should use the
+# lowest value that still uses less of the bandwidth your Internet connection
+# can handle.
 XZ_COMPRESS_FLAGS=(-5)
+
+# Command used with ssh to get a shell. You can change it e.g. to run commands
+# inside a chroot.
+REMOTE_BASH=(bash --login)  
+
+# Extra arguments passed to rsync. You can use these to execute rsync inside a
+# chroot.
 RSYNC_EXTRA_ARGS=()
 
-# For debugging
+# Only for debugging. It does prevent launching packet-receiver.sh when 
+# webkit-remote-build.sh is invoked. Useful if you want to run it in a separate
+# console tab.
 DO_NOT_LAUNCH_PACKET_RECEIVER_IN_REMOTE_BUILD=false
