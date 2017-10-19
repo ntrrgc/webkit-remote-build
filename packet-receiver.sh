6f1b083bef1150fbaec2f02e7ca9f21cdfed34f6
Alicia Boya García <ntrrgc@gmail.com>
2017-10-19 13:34:07 +0200

    Rename listen-socket to serial-listener

--- packet-receiver.sh
+++ packet-receiver.sh
@@ -10,5 +10,5 @@ ssh -T "$BUILD_HOST" <<END \
   | xargs -r -P$local_cores -d"\n" -n1 "$DIR/extract-packet.sh"
 set -eu
 rm -f /tmp/delta-socket
-${REMOTE_SCRIPTS_DIR@Q}/listen-socket.py /tmp/delta-socket | mbuffer -m 200M -q
+${REMOTE_SCRIPTS_DIR@Q}/serial-listener.py /tmp/delta-socket | mbuffer -m 200M -q
 END
