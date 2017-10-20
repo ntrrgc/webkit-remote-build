69338f5f23c876af106b88acafe0019e8f9a77df
Alicia Boya García <ntrrgc@gmail.com>
2017-10-20 12:36:07 +0200

    Add missing REMOTE_BASH argument

--- packet-receiver.sh
+++ packet-receiver.sh
@@ -5,7 +5,7 @@ DIR="$(dirname $(realpath "$0"))"
 . "$DIR/config.sh"
 
 local_cores="$(getconf _NPROCESSORS_ONLN)"
-ssh -T "$BUILD_HOST" <<END \
+ssh -T "$BUILD_HOST" "${REMOTE_BASH[@]}" <<END \
   | "$DIR/packet-splitter.sh" \
   | xargs -r -P$local_cores -d"\n" -n1 "$DIR/extract-packet.sh"
 set -eu
